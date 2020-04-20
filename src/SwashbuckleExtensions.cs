10096ea0307cb59cc156744a9fb99e0aa4998f8e
Anantawat, Ratchapol (Agoda) <Ratchapol.Anantawat@agoda.com>
2020-04-20 13:39:51 +0700

    init project

---
+++ src/SwashbuckleExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.Extensions.Filters;
+using System;
+
+namespace Swashbuckle.Extensions
+{
+    public static class SwashbuckleExtensions
+    {
+        public static void AddAutoRestCompatible(this SwaggerGenOptions options)
+        {
+            options.SchemaFilter<AutoRestSchemaFilter>();
+        }
+
+        public static void MakeValueTypePropertiesRequired(this SwaggerGenOptions options)
+        {
+            options.SchemaFilter<RequireValueTypePropertiesSchemaFilter>(true);
+        }
+
+        public static void DefineOperationIdFromControllerNameAndActionName(this SwaggerGenOptions options)
+        {
+            options.OperationFilter<OperationIdFilter>();
+        }
+    }
+}
